b94e0084cf37ca01310873584fba9f529d1b4718
chenp <chenpeleg@gmail.com>
2022-09-04 11:00:03 +0300

    updated to modern creat root

--- src/index.tsx
+++ src/index.tsx
@@ -1,28 +1,20 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './hoc/App';
 import reportWebVitals from './reportWebVitals';
 import {Root} from './hoc/Root';
 
-
-ReactDOM.render(
-    <Root>
-        <App/>
-    </Root>,
-    document.getElementById('root')
+const root = ReactDOM.createRoot(
+    document.getElementById('root') as HTMLElement
 );
+root.render(
+    <React.StrictMode>
+        <Root>
+            <App />
+        </Root>,
 
-// const root = ReactDOM.createRoot(
-//     document.getElementById('root') as HTMLElement
-// );
-// root.render(
-//     <React.StrictMode>
-//         <Root>
-//             <App />
-//         </Root>,
-//
-//     </React.StrictMode>
-// );
+    </React.StrictMode>
+);
 
 reportWebVitals();
