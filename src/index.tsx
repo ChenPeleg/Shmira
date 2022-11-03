cbd47a6d0a5061efafb3d18f4f6f1b19c45d4333
chenp <chenpeleg@gmail.com>
2022-11-03 13:00:13 +0200

    added react dom client

--- src/index.tsx
+++ src/index.tsx
@@ -1,28 +1,22 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import './index.css';
 import App from './hoc/App';
 import reportWebVitals from './reportWebVitals';
 import {Root} from './hoc/Root';
+import ReactDOM from 'react-dom/client';
 
 
-ReactDOM.render(
-    <Root>
-        <App/>
-    </Root>,
-    document.getElementById('root')
+
+const root = ReactDOM.createRoot(
+    document.getElementById('root') as HTMLElement
 );
+root.render(
+    <React.StrictMode>
+        <Root>
+            <App />
+        </Root>,
 
-// const root = ReactDOM.createRoot(
-//     document.getElementById('root') as HTMLElement
-// );
-// root.render(
-//     <React.StrictMode>
-//         <Root>
-//             <App />
-//         </Root>,
-//
-//     </React.StrictMode>
-// );
+    </React.StrictMode>
+);
 
 reportWebVitals();
